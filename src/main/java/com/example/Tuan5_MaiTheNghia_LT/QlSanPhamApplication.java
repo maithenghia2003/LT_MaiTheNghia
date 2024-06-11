019eaa0effa4f524c0596fa94caa8a8e18358a44
maithenghia2003 <maithenghia2506@gmail.com>
2024-06-12 01:36:57 +0700

    qlsanpham

---
+++ src/main/java/com/example/Tuan5_MaiTheNghia_LT/QlSanPhamApplication.java
@@ -0,0 +1,13 @@
+package com.example.Tuan5_MaiTheNghia_LT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlSanPhamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlSanPhamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Tuan5_MaiTheNghia_LT/QlSanPhamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tuan5_MaiTheNghia_LT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlSanPhamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
